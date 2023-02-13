26c691bfc5c8f4ac16999595c50cde5753912d4a
suyi_127 <9770329+suyi_127@user.noreply.gitee.com>
2023-02-13 20:52:07 +0800

    finish 01 with optimization

---
+++ 01.py
@@ -0,0 +1,41 @@
+# https://leetcode.cn/problems/is-unique-lcci/?favorite=xb9lfcwi
+
+class Solution:
+    def isUnique_1(self, astr: str) -> bool:
+        # 直接用字典进行去重即可
+        set_s = set(astr)
+        if len(set_s) < len(astr):
+            return False
+        else:
+            return True
+
+    def isUnique(self, astr: str) -> bool:
+        """
+        限制：
+        0 <= len(s) <= 100
+        s[i]仅包含小写字母
+        如果你不使用额外的数据结构，会很加分。
+
+        :param astr: str
+        :return: bool
+        """
+        # 哈希表策略
+        hash_dic = dict()
+        for i in range(ord('a'), ord('a')+26):
+            hash_dic[chr(i)] = 0
+
+        for s in astr:
+            hash_dic[s] += 1
+
+        if max(hash_dic.values()) > 1:
+            return False
+        else:
+            return True
+
+
+
+
+if __name__ == '__main__':
+    astr = "leetcode"
+    a = Solution()
+    print(a.isUnique(astr))
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# jianzhioffer
+### Interview
+
+
+
+重新上手代码
+
+leetcode地址
+
+https://leetcode.cn/problem-list/xb9lfcwi/?page=1
